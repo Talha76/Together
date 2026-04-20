e081ba881d8c27bb658f12dda722fc589328ca16
Talha76 <mushfiqurrahman845@gmail.com>
2026-04-20 22:55:06 +0600

    fix(download): use expo-file-system/legacy in useMessages

--- src/hooks/useMessages.js
+++ src/hooks/useMessages.js
@@ -1,6 +1,6 @@
 import { useState, useEffect, useRef, useCallback } from 'react';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import * as FileSystem from 'expo-file-system';
+import * as FileSystem from 'expo-file-system/legacy';
 import * as Sharing from 'expo-sharing';
 import { Buffer } from 'buffer';
 import {
